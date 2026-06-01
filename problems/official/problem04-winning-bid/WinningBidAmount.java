89129669158393d0c483b0dcb90c453e2d9e8994
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-06-01 09:44:35 +0900

    Update WinningBidAmount.java

--- problems/official/problem04-winning-bid/WinningBidAmount.java
+++ problems/official/problem04-winning-bid/WinningBidAmount.java
@@ -24,21 +24,17 @@ public class WinningBidAmount {
 	// amounts : 참가자 m명의 초기 자본을 담은 1차원 정수 배열
 
 	// answer : 물품 번호가 낮은 순으로 낙찰된 금액을 담은 길이가 n인 1차원 정수 배열
-
-	/*
-	 * 낙찰 금액 자본이 가장 많이 남은 참가자가 1명인 경우: 자본이 가장 많이 남은 참가자가 2명 이상인 경우:
-	 */
-
 	public int[] solution(int n, Integer[] amounts) {
 		int[] answer = new int[n];
 
 		for (int i = 0; i < n; i++) {
 			Arrays.sort(amounts, (o1, o2) -> o2 - o1);
 
+			// 자본이 가장 많이 남은 참가자가 2명 이상인 경우
 			if (amounts[0].equals(amounts[1])) {
 				answer[i] = amounts[0];
 				amounts[0] = 0;
-
+			// 낙찰 금액 자본이 가장 많이 남은 참가자가 1명인 경우
 			} else {
 				answer[i] = amounts[1] + 10000;
 				amounts[0] -= answer[i];
