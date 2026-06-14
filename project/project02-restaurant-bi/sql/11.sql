ec0f94cc3f57421b65f2f1706d1b12705b65e4a4
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-06-14 20:14:42 +0900

    Create 11.sql

---
+++ project/project02-restaurant-bi/sql/11.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM (
+    SELECT
+        SUBSTR(R.RESERV_DATE, 1, 6) AS 년월,
+        R.BRANCH AS 지점,
+        SUM(O.SALES) AS 매출액,
+
+        RANK() OVER (
+            PARTITION BY SUBSTR(R.RESERV_DATE, 1, 6)
+            ORDER BY SUM(O.SALES) DESC
+        ) AS 순위
+
+    FROM RESERVATION R
+    JOIN ORDER_INFO O
+        ON R.RESERV_NO = O.RESERV_NO
+    JOIN ITEM I
+        ON O.ITEM_ID = I.ITEM_ID
+
+    WHERE I.PRODUCT_DESC = '온라인_전용상품'
+
+    GROUP BY
+        SUBSTR(R.RESERV_DATE, 1, 6),
+        R.BRANCH
+)
+WHERE 순위 <= 3
+ORDER BY 년월, 순위;
